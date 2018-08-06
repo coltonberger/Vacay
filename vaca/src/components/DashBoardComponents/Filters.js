a65feb540876f141ea968d4c786dde1684ce5c89
Colton Berger <coltonberger@optonline.net>
2018-08-06 11:16:59 -0700

    added schedule on bottom of page and start and end time

--- vaca/src/components/DashBoardComponents/Filters.js
+++ vaca/src/components/DashBoardComponents/Filters.js
@@ -8,6 +8,7 @@ const Filters = ({ locations, selectCityFilter, selectPriceFilter }) => {
         <option className="disabled">Choose City</option>
         {locations.map(location => <option value={location}>{location}</option>)}
       </select>
+
       <select className="ui dropdown" id="filterPrice" onChange={selectPriceFilter}>
         <option className="disabled">Choose price/activity</option>
         <option value="">All</option>
@@ -15,6 +16,40 @@ const Filters = ({ locations, selectCityFilter, selectPriceFilter }) => {
         <option value="2">$20-$40</option>
         <option value="3">Greater than $40</option>
       </select>
+
+      <select className="ui dropdown" id="filterStartTime">
+        <option className="disabled">Start of Day</option>
+        <option value="">6am</option>
+        <option value="1">7am</option>
+        <option value="2">8am</option>
+        <option value="3">9am</option>
+        <option value="4">10am</option>
+        <option value="5">11am</option>
+        <option value="6">12am</option>
+        <option value="7">1pm</option>
+        <option value="8">2pm</option>
+        <option value="9">3pm</option>
+        <option value="10">4pm</option>
+        <option value="11">5pm</option>
+      </select>
+
+      <select className="ui dropdown" id="filterStartTime">
+        <option className="disabled">End of Day</option>
+        <option value="">12pm</option>
+        <option value="1">1pm</option>
+        <option value="2">2pm</option>
+        <option value="3">3pm</option>
+        <option value="4">4pm</option>
+        <option value="5">5pm</option>
+        <option value="6">6pm</option>
+        <option value="7">7pm</option>
+        <option value="8">8pm</option>
+        <option value="9">9pm</option>
+        <option value="10">10pm</option>
+        <option value="11">11pm</option>
+        <option value="12">12am</option>
+      </select>
+
       </div>
     );
 }
--- vaca/src/components/DashBoardComponents/Schedule.jsx
+++ vaca/src/components/DashBoardComponents/Schedule.jsx
@@ -5,7 +5,46 @@ class Schedule extends Component {
   render() {
     return (
       <div className="ui bottom fixed menu">
-        Schedule Component
+
+
+
+      <table class="ui celled table">
+        <thead>
+        <thead>
+          <tr><th>
+            Your Schedule
+          </th>
+        </tr></thead>
+          <tr>
+            <th>8am</th>
+            <th>9am</th>
+            <th>10am</th>
+            <th>11am</th>
+            <th>12pm</th>
+            <th>1pm</th>
+            <th>2pm</th>
+            <th>3pm</th>
+            <th>4pm</th>
+            <th>5pm</th>
+            <th>6pm</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+            <td>place event here</td>
+          </tr>
+        </tbody>
+      </table>
       </div>
     );
   }
