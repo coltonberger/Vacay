9fc3176308e9266ff81bcc1d8c78470f5858b7e1
Colton Berger <coltonberger@optonline.net>
2018-08-09 17:12:28 -0700

    added styling to buttons

--- vaca/src/components/DashBoardComponents/Filters.js
+++ vaca/src/components/DashBoardComponents/Filters.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import '../../App.css'
 
 const Filters = ({ locations, selectCityFilter, selectPriceFilter }) => {
   return (
     <div className='filters'>
+      <h3 className="ui center aligned header">vā kā</h3>
+        <div className="slogan">enjoy cities like a local</div>
 
       <h5>Create your Schedule</h5>
-      <select className="ui dropdown" id='filterLocation' onChange={selectCityFilter}>
+      <select className="ui dropdown button" id='filterLocation' onChange={selectCityFilter}>
         <option className='disabled'>Choose City</option>
           {/*{locations.map(location => <option key={location} value={location}>{location}</option>)}*/}
           <option value=''>All</option>
@@ -14,7 +17,7 @@ const Filters = ({ locations, selectCityFilter, selectPriceFilter }) => {
           <option value='3'>Los Angeles</option>
       </select>
 
-      <select className='ui dropdown' id='filterPrice' onChange={selectPriceFilter}>
+      <select className='ui dropdown button' id='filterPrice' onChange={selectPriceFilter}>
         <option className='disabled'>Choose price/activity</option>
         <option value=''>All</option>
         <option value='1'> Less than $20</option>
