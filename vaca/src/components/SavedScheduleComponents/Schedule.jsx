33f325c306972ee661e9a78f8e5cb7f79db6c35d
Colton Berger <coltonberger@optonline.net>
2018-08-10 17:56:53 -0700

    added styling to saved events

--- vaca/src/components/SavedScheduleComponents/Schedule.jsx
+++ vaca/src/components/SavedScheduleComponents/Schedule.jsx
@@ -4,13 +4,22 @@ import React, { Component } from 'react'
 class Schedule extends Component {
   render () {
     return (
-      <div className="ui divided items">
-        <div className="item">
-          <div className="middle aligned content">
-            {this.props.savedEvent.eventName}
+
+
+
+      <div class="ui cards">
+        <div class="card">
+          <div class="content">
+            <div class="header">{this.props.savedEvent.eventName}</div>
+            <div class="meta">{this.props.savedEvent.eventCity}</div>
+            <div class="description">
+              {this.props.savedEvent.eventDescription}
+            </div>
           </div>
         </div>
       </div>
+
+
     )
   }
 }
